048e78e3019c0fb702f8a4e4635233f7b3efa458
AyushDhamecha <144325067+AyushDhamecha@users.noreply.github.com>
2024-06-22 00:09:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react"
+import Header from "./component/Header.js"
+import Meme from "./component/Meme.js"
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Meme/>
+    </div>
+  )
+}
+
+
